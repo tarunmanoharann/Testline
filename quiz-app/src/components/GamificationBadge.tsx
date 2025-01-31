cb53576bd3f3b7f30447704936441be89ee81273
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2025-01-31 19:51:31 +0530

    FInal Version

--- quiz-app/src/components/GamificationBadge.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function GamificationBadge() {
-  return (
-    <div>GamificationBadge</div>
-  )
-}
-
-export default GamificationBadge
--- quiz-app/src/utils/api.ts
+++ quiz-app/src/utils/api.ts
@@ -1,23 +1,9 @@
 import { QuizData } from '../types/quizTypes';
 
 
-// export const fetchQuizData = async (): Promise<QuizData> => {
-//   try {
-//     const response = await fetch('/quizData.json');
-//     if (!response.ok) {
-//       throw new Error('Failed to fetch quiz data');
-//     }
-//     return response.json();
-//   } catch (error) {
-//     console.error('Error fetching quiz data:', error);
-//     throw error;
-//   }
-// };
-
-
 export const fetchQuizData = async (): Promise<QuizData> => {
   try {
-    const response = await fetch('http://localhost:5000/api/quiz');
+    const response = await fetch('/quizData.json');
     if (!response.ok) {
       throw new Error('Failed to fetch quiz data');
     }
@@ -27,3 +13,18 @@ export const fetchQuizData = async (): Promise<QuizData> => {
     throw error;
   }
 };
+
+
+//Local backend server
+// export const fetchQuizData = async (): Promise<QuizData> => {
+//   try {
+//     const response = await fetch('http://localhost:5000/api/quiz');
+//     if (!response.ok) {
+//       throw new Error('Failed to fetch quiz data');
+//     }
+//     return response.json();
+//   } catch (error) {
+//     console.error('Error fetching quiz data:', error);
+//     throw error;
+//   }
+// };
